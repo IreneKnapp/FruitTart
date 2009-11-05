e9fd65ea855eb2fe373229cae19e3497732d0ae7
Irene Knapp <irene.knapp@icloud.com>
2009-11-05 17:01:47 +0000

    Move getNavigationBar from the old, to-be-deleted-once-it's-empty Buglist.hs to the new Network.FruitTart.View.Navigation.

--- Buglist.hs
+++ Buglist.hs
@@ -191,39 +191,6 @@ getPageHeadItems
        ++ "<script src=\"/js/buglist.js\" type=\"text/ecmascript\"></script>\n")
 
 
-getNavigationBar :: String -> FruitTart String
-getNavigationBar currentPage = do
-  items <- query ("SELECT name, link, separator, always_enabled, class "
-                  ++ "FROM navigation_items ORDER BY id")
-                 []
-  let item name link separator alwaysEnabled maybeClass =
-          let class' = case maybeClass of
-                         Nothing -> ""
-                         Just class' -> " class=\"" ++ class' ++ "\""
-          in if separator
-             then "<div " ++ class' ++ "></div>"
-             else if (link /= currentPage) || alwaysEnabled
-                  then "<a href=\"" ++ (escapeAttribute link) ++ "\"" ++ class' ++ ">"
-                       ++ (escapeHTML name) ++ "</a>"
-                  else "<b" ++ class' ++ ">" ++ (escapeHTML name) ++ "</b>"
-  result <- return $ "<div id=\"navigation\">"
-           ++ (concat $ map (\[SQLText name,
-                               SQLText link,
-                               SQLInteger separator,
-                               SQLInteger alwaysEnabled,
-                               maybeClass]
-                             -> item name
-                                     link
-                                     (separator /= 0)
-                                     (alwaysEnabled /= 0)
-                                     (case maybeClass of
-                                        SQLNull -> Nothing
-                                        SQLText class' -> Just class'))
-                             items)
-           ++ "</div>\n"
-  return result
-
-
 getLoginButton :: String -> FruitTart String
 getLoginButton currentPage = do
   maybeUserID <- getLoggedInUser
