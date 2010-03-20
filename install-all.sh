9a3393ebb10136b38a2b8cef1c83340cb452a3f6
Irene Knapp <irene.knapp@icloud.com>
2010-03-20 01:19:41 +0000

    Add a script that invokes cabal install many times...

---
+++ install-all.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd FruitTartInterface
+cabal install
+cd ../FruitTart
+cabal install
+cd ../Base
+cabal install
+cd ../Captcha
+cabal install
+cd ../Buglist
+cabal install
+cd ../Blog
+cabal install
+cd ../Adventure
+cabal install
