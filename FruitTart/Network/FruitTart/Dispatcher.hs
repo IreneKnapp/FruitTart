a9afe709376ccc1697a3092f20ba32b8cf2ecf1c
Irene Knapp <irene.knapp@icloud.com>
2010-03-20 01:14:50 +0000

    Change content type from html to xhtml.

--- FruitTart/Network/FruitTart/Dispatcher.hs
+++ FruitTart/Network/FruitTart/Dispatcher.hs
@@ -34,7 +34,7 @@ processRequest' dispatchTable = do
   initializeSession
   processFormInput
   callModuleInitRequestMethods
-  setResponseHeader HttpContentType "text/html; charset=UTF8"
+  setResponseHeader HttpContentType "text/xhtml; charset=UTF8"
   maybeURL <- getQueryString
   let url = fromJust maybeURL
   (controllerName, actionName, urlParameters, namedParameters)
