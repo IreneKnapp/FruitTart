0ae1ab6b67c0694ffcf3d0cd7d6023dbea0dfb0a
Irene Knapp <irene.knapp@icloud.com>
2010-03-21 16:21:58 +0000

    Put some &&s in install-all.sh.

--- install-all.sh
+++ install-all.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-cd FruitTartInterface
-cabal install
-cd ../FruitTart
-cabal install
-cd ../Base
-cabal install
-cd ../Captcha
-cabal install
-cd ../Buglist
-cabal install
-cd ../Blog
-cabal install
-cd ../Adventure
-cabal install
+cd ~/Projects/FruitTart && \
+cd FruitTartInterface && \
+cabal install && \
+cd ../FruitTart && \
+cabal install && \
+cd ../Base && \
+cabal install && \
+cd ../Captcha && \
+cabal install && \
+cd ../Buglist && \
+cabal install && \
+cd ../Blog && \
+cabal install && \
+cd ../Adventure && \
+cabal install && \
+echo "\n\nDONE!!!\n"
