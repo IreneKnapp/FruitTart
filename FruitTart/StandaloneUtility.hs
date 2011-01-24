57a8e8b6335cbe3f18c016c417aaae8d428ffec6
Irene Knapp <irene.knapp@icloud.com>
2011-01-24 17:16:31 +0000

    Improvements to FruitTartUtility to dump functions, actions, templates, and queries, not just functions.

--- FruitTart/StandaloneUtility.hs
+++ FruitTart/StandaloneUtility.hs
@@ -22,8 +22,8 @@ main :: IO ()
 main = do
   databasePath <- getEnv "FRUITTART_DB"
   database <- open databasePath
+  
   functionsFile <- openFile "functions.txt" WriteMode
-  templatesFile <- openFile "templates.txt" WriteMode
   functions
     <- earlyQuery database
                   "SELECT id, module, name FROM functions ORDER BY module, name"
@@ -32,28 +32,166 @@ main = do
            let headerLine = moduleName ++ "." ++ properName ++ ": " ++ show id
                headerStandoffLine
                  = replicate (length headerLine) '='
-           putStrLn headerStandoffLine
-           putStrLn headerLine
-           putStrLn headerStandoffLine
-           putStrLn ""
+           hPutStrLn functionsFile headerStandoffLine
+           hPutStrLn functionsFile headerLine
+           hPutStrLn functionsFile headerStandoffLine
+           hPutStrLn functionsFile ""
+           actions
+             <- earlyQuery database
+                           ("SELECT actions.id, actions.method, "
+                            ++ "controllers.mapped_name, actions.action "
+                            ++ "FROM controller_actions AS actions "
+                            ++ "LEFT JOIN controllers as controllers "
+                            ++ "ON actions.controller = controllers.module "
+                            ++ "WHERE actions.controller = ? "
+                            ++ "AND actions.function = ? "
+                            ++ "ORDER BY actions.method, "
+                            ++ "controllers.mapped_name, actions.action")
+                           [SQLText moduleName, SQLText properName]
+           mapM_ (\[SQLInteger actionID, SQLText method, SQLText mappedName,
+                    SQLText actionName] -> do
+                    hPutStrLn functionsFile
+                              $ method
+                                ++ " /" ++ mappedName
+                                ++ "/" ++ actionName
+                    mandatoryParameters
+                      <- earlyQuery database
+                                    ("SELECT type "
+                                     ++ "FROM action_mandatory_parameters "
+                                     ++ "WHERE action = ? "
+                                     ++ "ORDER BY item")
+                                    [SQLInteger actionID]
+                    optionalParameters
+                      <- earlyQuery database
+                                    ("SELECT type "
+                                     ++ "FROM action_optional_parameters "
+                                     ++ "WHERE action = ? "
+                                     ++ "ORDER BY item")
+                                    [SQLInteger actionID]
+                    namedParameters
+                      <- earlyQuery database
+                                    ("SELECT type, name "
+                                     ++ "FROM action_named_parameters "
+                                     ++ "WHERE action = ? "
+                                     ++ "ORDER BY name")
+                                    [SQLInteger actionID]
+                    let parametersLine
+                          = "  " ++ (intercalate ", "
+                            $ concat [map (\[SQLText typeName] -> typeName)
+                                          mandatoryParameters,
+                                      case map (\[SQLText typeName] -> typeName)
+                                               optionalParameters of
+                                        [] -> []
+                                        (headType:restTypes)
+                                          -> (":optional " ++ headType)
+                                             : restTypes,
+                                      case map (\[SQLText typeName,
+                                                  SQLText name] ->
+                                                  typeName ++ " " ++ name)
+                                               namedParameters of
+                                        [] -> []
+                                        (headItem:restItems)
+                                          -> (":key " ++ headItem)
+                                             : restItems])
+                    hPutStrLn functionsFile parametersLine
+                    hPutStrLn functionsFile "")
+                 actions
            parameters
              <- earlyQuery database
                            ("SELECT name FROM function_parameters "
                             ++ "WHERE function = ? ORDER BY item")
                            [SQLInteger id]
            if null parameters
-             then putStrLn "(No parameters.)"
-             else mapM_ (\[SQLText parameterName] -> putStrLn parameterName)
+             then hPutStrLn functionsFile "(No parameters.)"
+             else mapM_ (\[SQLText parameterName] -> do
+                           hPutStrLn functionsFile parameterName)
                         parameters
-           putStrLn ""
+           hPutStrLn functionsFile ""
            [[SQLText body]]
              <- earlyQuery database
                            "SELECT body FROM functions WHERE id = ?"
                            [SQLInteger id]
-           putStrLn ">>>>>"
-           putStrLn body
-           putStrLn "<<<<<"
-           putStrLn ""
-           putStrLn "")
+           hPutStrLn functionsFile ">>>>>"
+           hPutStrLn functionsFile body
+           hPutStrLn functionsFile "<<<<<"
+           hPutStrLn functionsFile ""
+           hPutStrLn functionsFile "")
         functions
-  return ()
+  hClose functionsFile
+  
+  templatesFile <- openFile "templates.txt" WriteMode
+  templates
+    <- earlyQuery database
+                  "SELECT id, module, name FROM templates ORDER BY module, name"
+                  []
+  mapM_ (\[SQLInteger id, SQLText moduleName, SQLText properName] -> do
+           let headerLine = moduleName ++ "." ++ properName ++ ": " ++ show id
+               headerStandoffLine
+                 = replicate (length headerLine) '='
+           hPutStrLn templatesFile headerStandoffLine
+           hPutStrLn templatesFile headerLine
+           hPutStrLn templatesFile headerStandoffLine
+           hPutStrLn templatesFile ""
+           hPutStrLn templatesFile ">>>>>"
+           items <- earlyQuery database
+                               ("SELECT kind, body FROM template_items "
+                                ++ "WHERE template = ? ORDER BY item")
+                               [SQLInteger id]
+           mapM_ (\[SQLText kind, SQLText body] -> do
+                    let kindMarker = case kind of
+                                       "content" -> '*'
+                                       "expression" -> '='
+                        bodyLines = case lines body of
+                                  [] -> [""]
+                                  [line] -> [line]
+                                  bodyLines ->
+                                    reverse $ tail $ reverse bodyLines
+                        markedLines = ([kindMarker] ++ head bodyLines)
+                                      : (map (\line -> " " ++ line)
+                                             $ tail bodyLines)
+                        markedBody = unlines markedLines
+                    hPutStrLn templatesFile markedBody)
+                 items
+           hPutStrLn templatesFile "<<<<<"
+           hPutStrLn templatesFile ""
+           hPutStrLn templatesFile "")
+        templates
+  hClose templatesFile
+  
+  queriesFile <- openFile "queries.txt" WriteMode
+  functions
+    <- earlyQuery database
+                  "SELECT id, module, name FROM queries ORDER BY module, name"
+                  []
+  mapM_ (\[SQLInteger id, SQLText moduleName, SQLText properName] -> do
+           let headerLine = moduleName ++ "." ++ properName ++ ": " ++ show id
+               headerStandoffLine
+                 = replicate (length headerLine) '='
+           hPutStrLn queriesFile headerStandoffLine
+           hPutStrLn queriesFile headerLine
+           hPutStrLn queriesFile headerStandoffLine
+           hPutStrLn queriesFile ""
+           results
+             <- earlyQuery database
+                           ("SELECT type, name FROM query_results "
+                            ++ "WHERE query = ? ORDER BY item")
+                           [SQLInteger id]
+           if null results
+             then hPutStrLn queriesFile "(No results.)"
+             else mapM_ (\[SQLText resultType, SQLText resultName] -> do
+                           hPutStrLn queriesFile
+                                     $ resultType ++ " " ++ resultName)
+                        results
+           hPutStrLn queriesFile ""
+           [[SQLText body]]
+             <- earlyQuery database
+                           "SELECT body FROM queries WHERE id = ?"
+                           [SQLInteger id]
+           hPutStrLn queriesFile ">>>>>"
+           hPutStrLn queriesFile body
+           hPutStrLn queriesFile "<<<<<"
+           hPutStrLn queriesFile ""
+           hPutStrLn queriesFile "")
+        functions
+  hClose queriesFile
+  
