7dc2c3ed316c6f129d974225a301792b96367043
Irene Knapp <irene.knapp@icloud.com>
2011-01-25 15:51:54 +0000

    Implemented the Base.Maybe functions.

--- FruitTart/Network/FruitTart/Custard/Functions/Maybe.hs
+++ FruitTart/Network/FruitTart/Custard/Functions/Maybe.hs
@@ -42,8 +42,14 @@ cfMaybe :: CustardContext
         -> [CustardValue]
         -> FruitTart (CustardContext, CustardValue)
 cfMaybe context parameters = do
-  error "Not yet implemented."
-  -- TODO
+  requireNParameters parameters 2 "maybe"
+  defaultValue <- return $ parameters !! 0
+  action <- valueToMonadicAction $ parameters !! 1
+  maybeValue <- return $ parameters !! 2
+  case maybeValue of
+    CustardMaybe Nothing -> return (context, defaultValue)
+    CustardMaybe (Just actualValue) -> action context [actualValue]
+    _ -> error $ "Parameter is not a Maybe in maybe()."
 
 
 cfIsJust :: CustardContext
@@ -87,37 +93,78 @@ cfFromMaybe :: CustardContext
             -> [CustardValue]
             -> FruitTart (CustardContext, CustardValue)
 cfFromMaybe context parameters = do
-  error "Not yet implemented."
-  -- TODO
+  requireNParameters parameters 2 "fromMaybe"
+  defaultValue <- return $ parameters !! 0
+  maybeValue <- return $ parameters !! 1
+  case maybeValue of
+    CustardMaybe Nothing -> return (context, defaultValue)
+    CustardMaybe (Just actualValue) -> return (context, actualValue)
+    _ -> error $ "Parameter is not a Maybe in fromMaybe()."
 
 
 cfListToMaybe :: CustardContext
               -> [CustardValue]
               -> FruitTart (CustardContext, CustardValue)
 cfListToMaybe context parameters = do
-  error "Not yet implemented."
-  -- TODO
+  requireNParameters parameters 1 "listToMaybe"
+  value <- return $ parameters !! 0
+  case value of
+    CustardList [] -> return (context, CustardMaybe Nothing)
+    CustardList (head:_) -> return (context, CustardMaybe $ Just head)
+    _ -> error $ "Parameter is not a List in listToMaybe()."
 
 
 cfMaybeToList :: CustardContext
               -> [CustardValue]
               -> FruitTart (CustardContext, CustardValue)
 cfMaybeToList context parameters = do
-  error "Not yet implemented."
-  -- TODO
+  requireNParameters parameters 1 "maybeToList"
+  value <- return $ parameters !! 0
+  case value of
+    CustardMaybe Nothing
+      -> return (context, CustardList [])
+    CustardMaybe (Just result) -> return (context, CustardList [result])
+    _ -> error $ "Parameter is not a Maybe in maybeToList()."
 
 
 cfCatMaybes :: CustardContext
             -> [CustardValue]
             -> FruitTart (CustardContext, CustardValue)
 cfCatMaybes context parameters = do
-  error "Not yet implemented."
-  -- TODO
+  requireNParameters parameters 1 "catMaybes"
+  value <- return $ parameters !! 0
+  case value of
+    (CustardList []) -> return (context, CustardList [])
+    (CustardList items@(CustardMaybe _ : _)) ->
+      return (context,
+              CustardList
+               $ foldl (\result maybeValue ->
+                          case maybeValue of
+                            CustardMaybe Nothing -> result
+                            CustardMaybe (Just something) -> result ++ [something])
+                       []
+                       items)
+    _ -> error "Parameter is not a List of Maybes in catMaybes()."
 
 
 cfMapMaybe :: CustardContext
            -> [CustardValue]
            -> FruitTart (CustardContext, CustardValue)
 cfMapMaybe context parameters = do
-  error "Not yet implemented."
-  -- TODO
+  requireNParameters parameters 2 "mapMaybe"
+  action <- valueToMonadicAction $ parameters !! 0
+  items <- return $ parameters !! 1
+  case items of
+    (CustardList items) -> do
+       (context, result)
+         <- foldM (\(context, result) item -> do
+                     (context, itemResult) <- action context [item]
+                     case itemResult of
+                       CustardMaybe Nothing -> return (context, result)
+                       CustardMaybe (Just something) ->
+                         return (context, result ++ [something])
+                       _ -> error "Function result is not a Maybe in mapMaybe().")
+                  (context, [])
+                  items
+       return (context, CustardList result)
+    _ -> error "Parameter is not a List in mapMaybe()."
